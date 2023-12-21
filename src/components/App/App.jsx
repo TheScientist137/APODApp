f226d452daa87f86f2061c45a085c462877c9e4d
The-Scientist <thescientist137@outlook.es>
2023-12-21 09:43:02 +0100

    Finished visualization missing colors and transitions

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,54 @@
-import React, { useEffect, useState } from 'react'
-import getApodData from '../util/apod'
+import { useEffect, useState } from 'react'
+import { getApodData } from '../../util/apod'
+import { IoIosHeart } from 'react-icons/io';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+
+import './App.css'
 
 function App() {
+   const today = new Date().toISOString().split('T')[0]
    const [apodData, setApodData] = useState({})
+   const [date, setNewDate] = useState(today)
+
+   const changeDay = (day) => {
+      const currentDate = new Date(date)
+      currentDate.setDate(currentDate.getDate() + day)
+      const formatDay = currentDate.toISOString().split('T')[0]
+      setNewDate(formatDay)
+   }
 
    useEffect(() => {
-      getApodData()
+      getApodData(date)
          .then(data => setApodData(data))
          .catch(error => console.log('error to obtain api data', error))
-   }, [])
+   }, [date])
 
    return (
-      <div className='app'>
-         <h1>Astronomy Picture of the Day</h1>
-         <h2>{apodData.title}</h2>
-         <img src={apodData.url} alt="image" />
-         <p>{apodData.date}</p>
-         <p>{apodData.explanation}</p>
-         <p>{apodData.copyright}</p>
-      </div>
+      <main className='app'>
+         <h1 className='app-title'>Astronomy Picture of the Day</h1>
+         <div className='image-container'>
+            <a href={apodData.url} className='image-link'><h2 className='image-title'>{apodData.title}</h2></a>
+            <img src={apodData.url} alt={apodData.copyright} className='image' />
+         </div>
+         <div className='description-container'>
+            <p className='explanation'>{apodData.explanation}</p>
+            <h3 className='author'>{apodData.copyright}</h3>
+            <p className='date'>{apodData.date}</p>
+         </div>
+         <div className='change-image-container'>
+            <button className='change-image' onClick={() => changeDay(-1)}>{'<'}</button>
+            {date === today
+            ? <button className='hidden'>{}</button>
+            : <button className='change-image' onClick={() => changeDay(1)}>{'>'}</button> }
+         </div>
+         <footer className='footer'>
+            <h5 className='code-author'>Made with  <IoIosHeart className='heart' />  by Guillermo Casado</h5>
+            <div className='icon-container'>
+               <a href='https://www.linkedin.com/in/guillermo-casado-noya-209b45268/'><FaLinkedin className='icon linkedin' /></a>
+               <a href='https://github.com/TheScientist137'><FaGithub className='icon' /></a>
+            </div>
+         </footer>
+      </main>
    )
 }
 export default App
--- src/util/apod.js
+++ src/util/apod.js
@@ -1,14 +1,12 @@
-const api_key = 'HsLqh3Cj31zxheq43H69jo1uU85Oxarg7JGcAjwF';
+const api_key = 'HsLqh3Cj31zxheq43H69jo1uU85Oxarg7JGcAjwF'
 
-const getApodData = async () => {
-    try {
-        const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${api_key}`);
-        if (!response.ok) {
-            throw new Error('Error to obtain data from api');
-        }
-        const data = await response.json();
-        return data;
-    } catch (error) { throw error }
+export const getApodData = async (date) => {
+  const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${api_key}&date=${date}`)
+
+  if (!response.ok) { throw new Error('Error to obtain data from api') }
+
+  const data = await response.json()
+  console.log(data)
+
+  return data
 }
-
-export default getApodData;
